5841b698cab412f85032f6044ff0942d4167204f
Arafatul Islam <https://github.com/Arafat-niloy>
2025-11-13 18:53:50 +0600

    netlify issue test-5

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
     'http://localhost:5176',
-    'https://jade-tulumba-222b98.netlify.app/'
+    'https://jade-tulumba-222b98.netlify.app'
   ],
   optionsSuccessStatus: 200,
 };
